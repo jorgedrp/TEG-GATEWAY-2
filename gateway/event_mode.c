c1962fd04bc9fc72ceb2c99da5aaf800a8e57f30
Jorge Ramirez <jorgedrplus@gmail.com>
2026-08-26 00:08:11 -0400

    Update event_mode.c

--- gateway/event_mode.c
+++ gateway/event_mode.c
@@ -248,6 +248,9 @@ void* task_communicator(void* p)
             {
                 retardo_milisegundos((long)(120000 / num_sensores));
 
+                printf("SENSOR:%u | CONECTANDO...\n", sensor_list[k].dev_id);
+                fflush(stdout);
+
                 uint8_t data[PAYLOAD_TX_LENGTH] = {sensor_list[k].dev_id, STATUS_CODE, 0xFF, 0xFF, 0xFF};
                 send_packet(data, PAYLOAD_TX_LENGTH);
 
